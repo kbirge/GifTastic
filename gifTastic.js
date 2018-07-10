ae127bc003f378557c920e3a8f33c8b3d8c308d5
Katie <kmbirge@gmail.com>
2018-07-10 12:50:31 -0500

    added search bar and tried to sudo code some js

---
+++ gifTastic.js
@@ -0,0 +1,11 @@
+//API url
+
+
+// Set API key
+//var (?) = {"api-key": "ygVAsVC3wYsod0RtS9GcxtWXKyFRJuO3"}
+
+//topics variable 
+var topice = new Array("dog", "cat", "sloth", "goat", "fish");
+var numberOfGIFs = 10;
+
+
